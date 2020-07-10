36533eea08ec09bdef636ad8ac019837c391bf14
Eldar <eldar7989@gmail.com>
2020-07-10 22:06:01 +0200

    add redirect pages after registration and message about added dish

--- src/main/java/com/example/restaurant/repository/UserRepository.java
+++ src/main/java/com/example/restaurant/repository/UserRepository.java
@@ -1,6 +1,7 @@
 package com.example.restaurant.repository;
 
 import com.example.restaurant.entity.User;
+import com.example.restaurant.enums.UserRoles;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
@@ -10,4 +11,9 @@ public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findUserById(Long id);
 
     Optional<User> findByName(String name);
+
+    User findByUserRole(UserRoles userRole);
+
+    boolean existsByUserRole(UserRoles userRole);
+
 }
