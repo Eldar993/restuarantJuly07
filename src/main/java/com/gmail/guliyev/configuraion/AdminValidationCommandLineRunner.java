1a6a23c39a87f6414cd4afcfeb4e45c8b135aada
Eldar <eldar@email.com>
2020-07-11 23:28:00 +0300

    Logging created admin's credentials

--- src/main/java/com/gmail/guliyev/configuraion/AdminValidationCommandLineRunner.java
+++ src/main/java/com/gmail/guliyev/configuraion/AdminValidationCommandLineRunner.java
@@ -4,6 +4,8 @@ package com.gmail.guliyev.configuraion;
 import com.gmail.guliyev.entity.User;
 import com.gmail.guliyev.enums.UserRoles;
 import com.gmail.guliyev.service.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.ApplicationContextException;
 import org.springframework.stereotype.Component;
@@ -11,6 +13,7 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class AdminValidationCommandLineRunner implements CommandLineRunner {
+    private static final Logger LOG = LoggerFactory.getLogger(AdminValidationCommandLineRunner.class);
     private final UserService userService;
 
     public AdminValidationCommandLineRunner(UserService userService) {
@@ -19,21 +22,24 @@ public class AdminValidationCommandLineRunner implements CommandLineRunner {
 
     @Override
     public void run(String... args) {
+        LOG.trace("Running AdminValidation component...");
         if (userService.existsByUserRole(UserRoles.ADMIN)) {
+            LOG.debug("Ok! Admin(-s) is present.");
             return;
         }
         User admin = new User();
         admin.setName(userService.generateRandomString(5));
-        admin.setPassword(userService.generateRandomString(5));
+        final String rawPassword = userService.generateRandomString(5);
+        admin.setPassword(rawPassword);
         boolean result = userService.create(admin, UserRoles.ADMIN);
-        if(result){
-            System.out.println(admin.getName());
-            System.out.println(admin.getPassword());
-        }else{
+        if (result) {
+            LOG.info("Generated admin's credentials: \n" +
+                            "   login:    {}\n" +
+                            "   password: {}",
+                    admin.getName(), rawPassword);
+        } else {
             throw new ApplicationContextException("Admin not found!");
         }
-
-
     }
 }
 
